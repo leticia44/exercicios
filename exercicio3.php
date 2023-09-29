e8405425ebed9e4cf49a5cbd71ea3c83692bf52c
12leticiasilva0990 <130669115+12leticiasilva0990@users.noreply.github.com>
2023-09-29 11:38:31 -0300

    commit lety

--- exercicio3.php
+++ exercicio3.php
@@ -8,9 +8,9 @@ if (is_numeric($num1)) {
     $result = $num1 * $num2;
     if ($result > 100) {
     
-        echo " ($resultado) É maior que 100.";
+        echo " ($result) É maior que 100.";
     } else {
-        echo "($resultado) Não é maior que 100.";
+        echo "($result) Não é maior que 100.";
     }
 } else {
     echo "A variável não é um número.";
