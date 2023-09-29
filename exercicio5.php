6172f980ec753e436fe1237a04ead51c2612e706
12leticiasilva0990 <130669115+12leticiasilva0990@users.noreply.github.com>
2023-09-29 15:34:57 -0300

    commit lety

--- exercicio5.php
+++ exercicio5.php
@@ -1,16 +1,16 @@
 <?php
 //declarando variáveis
 $num = 4;
-$limite * 30;
+$limite = 30;
 // enquanto número for menor que 38
 while($num < $limite) {
 
 // Exibir os números
- echo "Executando o loop $num <br>";
+ echo "Executando $num <br>";
 
 // Se o número for idêntico a 24
 if($num === 24) {
-    echo "Parando o loop";
+    echo "Parando o Loop✋";
 break;
 }
 $num += 2;
