c624af7adc532220175eb757dc3430d858930b44
12leticiasilva0990 <130669115+12leticiasilva0990@users.noreply.github.com>
2023-09-29 09:11:34 -0300

    first commit

---
+++ exercicio1.php
@@ -0,0 +1,34 @@
+<?php
+//Variaveis
+$numero1 = 5;
+$numero2 = 2;
+$pessoa1 = "Robson";
+$pessoa2 = "Pedro"; 
+$numero3 = 12;
+$numero4 = 11;
+
+if ($numero1 > $numero2) {
+    echo "$numero1 é maior que $numero2 <br>";
+
+} else{ 
+    echo "$numero1 não é maior que $numero2";
+}
+
+if ($pessoa1!=$pessoa2) {
+    echo "$pessoa1 é diferente que $pessoa2 <br>";
+
+} else{ 
+    echo "$pessoa1 não é diferente que $pessoa2";
+}
+if ($numero3  <= $numero4) {
+    echo "$numero3 é menor ou igual ou $numero4 <br>";
+
+} else{ 
+    echo "$numero3 não é igual ou menor que $numero4";
+}
+
+
+
+
+
+?>
