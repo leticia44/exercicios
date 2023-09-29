c375dbf891551cd38dc04580afebbefbe7f69c93
12leticiasilva0990 <130669115+12leticiasilva0990@users.noreply.github.com>
2023-09-29 13:33:57 -0300

    commit lety

---
+++ exercicio4.php
@@ -0,0 +1,18 @@
+<?php
+ $velocidade = 45;
+
+ if ($velocidade >= 41 && $velocidade <= 50){
+    echo "O motorista está na velocidade correta <br>";
+
+    }else if($velocidade < 40 ){
+    echo "ALERTA!<br>";
+
+    }else if ($velocidade >50){
+    echo "Multa!<br>";
+}
+
+
+
+ 
+ 
+?>
